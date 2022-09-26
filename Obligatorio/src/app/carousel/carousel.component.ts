05799b6d1c2f6f10b2c5fef4057e22482705a644
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-09-26 10:23:53 -0300

    Header - Footer - Carousel

---
+++ Obligatorio/src/app/carousel/carousel.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
+export class CarouselComponent implements OnInit {
+
+  images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
